7bc5866b3b364d85a13fb9fee9fa2ce0f35db361
Tibi <bukkfabiszti@gmail.com>
2018-08-23 18:43:36 +0200

    PokerClient refactored. Still an issue with tests, will have to talk with mentors.

--- src/PokerClient.java
+++ src/PokerClient.java
@@ -1,54 +1,33 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class PokerClient {
+class PokerClient {
 
-    public Card card1;
-    public Card card2;
-    public Card card3;
-    public Card card4;
-    public Card card5;
+private List<Card> opponentCards = new ArrayList<>();
 
-    public PokerClient(String p1, String p2, String p3, String p4, String p5) {
-        this.card1 = new Card(p1.toUpperCase());
-        this.card2 = new Card(p2.toUpperCase());
-        this.card3 = new Card(p3.toUpperCase());
-        this.card4 = new Card(p4.toUpperCase());
-        this.card5 = new Card(p5.toUpperCase());
+    PokerClient(String p1, String p2, String p3, String p4, String p5) {
+        List<String> params = Arrays.asList(p1, p2, p3, p4, p5);
+        for (String cardCode: params) {
+            opponentCards.add(new Card(cardCode));}
     }
 
-    public boolean highestCardIsMine(String p1, String p2, String p3, String p4, String p5) {
-        Card hc = new Card("s2");
-        List<Card> o = new ArrayList<Card>();
-        List<Card> m = new ArrayList<Card>();
-        o.add(new Card(p1.toUpperCase()));
-        o.add(new Card(p2.toUpperCase()));
-        o.add(new Card(p3.toUpperCase()));
-        o.add(new Card(p4.toUpperCase()));
-        o.add(new Card(p5.toUpperCase()));
-        m.add(card1);
-        m.add(card2);
-        m.add(card3);
-        m.add(card4);
-        m.add(card5);
+    boolean highestCardIsMine(String p1, String p2, String p3, String p4, String p5) {
+        List<Card> myCards = new ArrayList<>();
 
-        for (int i = 0; i < o.size(); i++) {
-            Card mc = m.get(i);
-            for (int j = 0; j < o.size(); j++) {
-                Card oc = o.get(j);
-                if (oc.getValue() > mc.getValue()) {
-                    if (oc.getValue() > hc.getValue()) {
-                        hc = oc;
-                    }
-                } else {
-                    if (mc.getValue() > hc.getValue()) {
-                        hc = mc;
-                    }
-                }
-            }
-        }
+        List<String> paramList = Arrays.asList(p1, p2, p3, p4, p5);
+        for (String cardCode: paramList) {myCards.add(new Card(cardCode));}
 
-        return m.contains(hc);
+        return
+                myCards.stream()
+                        .map(card -> card.cardValue)
+                        .max(Integer::compareTo)
+                        .orElse(0)
+                        >
+                opponentCards.stream()
+                        .map(card -> card.cardValue)
+                        .max(Integer::compareTo)
+                        .orElse(0);
     }
 
 }
--- tests/PokerClientTest.java
+++ tests/PokerClientTest.java
@@ -8,9 +8,9 @@ class PokerClientTest {
         PokerClient pokerClientTest;
 
         pokerClientTest = new PokerClient("h3", "d2", "s3", "s4", "cq");
-        assertTrue(pokerClientTest.highestCardIsMine("s3", "h4", "s5", "d6", "sj"));
+        assertTrue(pokerClientTest.highestCardIsMine("s2", "h4", "s5", "d6", "sj"));
 
         pokerClientTest = new PokerClient("h3", "d2", "s3", "s4", "cq");
-        assertFalse(pokerClientTest.highestCardIsMine("s3", "h4", "s5", "d6", "sk"));
+        assertFalse(pokerClientTest.highestCardIsMine("d3", "h5", "s4", "d7", "sk"));
     }
 }
